d975452be4367dcbea82cf22f68fded49f4db474
XuanDong <22130043@st.hcmuaf.edu.vn>
2025-10-08 16:02:46 +0700

    first commit

---
+++ backend/src/main/java/com/example/model/User.java
@@ -0,0 +1,17 @@
+package com.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity (name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,85 @@
+
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
+import '@ionic/react/css/palettes/dark.system.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+import { IonApp, IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { homeOutline, checkmarkDoneOutline, searchOutline, settingsOutline, addCircle } from 'ionicons/icons';
+import { Route, Redirect } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+setupIonicReact();
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route path="/home" component={Home} exact />
+          {/* <Route path="/finished" component={Finished} exact />
+          <Route path="/search" component={Search} exact />
+          <Route path="/settings" component={Settings} exact /> */}
+          <Redirect exact from="/" to="/home" />
+        </IonRouterOutlet>
+
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="home" href="/home">
+            <IonIcon icon={homeOutline} />
+            <IonLabel>Home</IonLabel>
+          </IonTabButton>
+
+          <IonTabButton tab="finished" href="/finished">
+            <IonIcon icon={checkmarkDoneOutline} />
+            <IonLabel>Finished</IonLabel>
+          </IonTabButton>
+
+          {/* Nút tròn ở giữa */}
+          <IonTabButton tab="add" onClick={() => alert('Add new note')}>
+            <IonIcon icon={addCircle} style={{ fontSize: '48px', marginTop: '-16px' }} />
+          </IonTabButton>
+
+          <IonTabButton tab="search" href="/search">
+            <IonIcon icon={searchOutline} />
+            <IonLabel>Search</IonLabel>
+          </IonTabButton>
+
+          <IonTabButton tab="settings" href="/settings">
+            <IonIcon icon={settingsOutline} />
+            <IonLabel>Settings</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
